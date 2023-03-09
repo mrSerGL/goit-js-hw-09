ad37bb4867c83f66a929bc5b70f8c4f443a20d81
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-03-09 14:31:19 +0200

    mission 1 complit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  bodyRef: document.querySelector('body'),
+  btnStartRef: document.querySelector('button[data-start]'),
+  btnStopRef: document.querySelector('button[data-stop]'),
+};
 
+let currentBodyColor = getRandomHexColor();
+refs.bodyRef.style.backgroundColor = currentBodyColor;
+
+refs.btnStartRef.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    refs.bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.btnStartRef.disabled = true;
+});
+
+refs.btnStopRef.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.btnStartRef.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
