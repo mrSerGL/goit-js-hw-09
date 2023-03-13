c734fd5ce6b0e09ae63d03454d82a0b3376f0d9b
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-03-13 08:16:17 +0200

    connect notify library on promises page

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const formRef = document.querySelector('.form');
 const delayInputRef = formRef.elements['delay'];
 const stepInputRef = formRef.elements['step'];
@@ -46,9 +48,11 @@ if (shouldResolve) {
 promise
 .then(({ position, delay }) => {
   console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 })
 .catch(({ position, delay }) => {
   console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 });
   }, currentDelay);
 }
